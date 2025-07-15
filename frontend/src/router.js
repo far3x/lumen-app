88db3f8641424a75a6f6b223a84f7912cc737a3a
Far3000-YT <far3000yt@gmail.com>
2025-07-15 23:40:44 +0200

    landing page v2

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -51,11 +51,11 @@ function setupScrollAnimations() {
         entries.forEach(entry => {
             if (entry.isIntersecting) {
                 entry.target.classList.add('scrolled-in');
+                observer.unobserve(entry.target);
             }
         });
     }, {
-        threshold: 0.1,
-        rootMargin: '0px 0px -50px 0px'
+        threshold: 0.1
     });
 
     animatedElements.forEach(el => {
