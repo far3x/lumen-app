7d6aca27008d9916a9af1a9e23852bc658741389
Far3000-YT <far3000yt@gmail.com>
2025-10-31 13:12:07 +0100

    need rpc url fix

--- business-frontend/src/lib/api.js
+++ business-frontend/src/lib/api.js
@@ -1,64 +1,10 @@
 import axios from 'axios';
 import { logout } from './auth';
-import { withPaymentInterceptor } from '@payai/x402-axios';
-import { walletService } from './wallet.js';
 
-const baseAxios = axios.create({
+const api = axios.create({
   baseURL: (import.meta.env.VITE_API_URL || 'http://localhost:8000') + '/api/v1',
   withCredentials: true,
 });
-
-const TransactionSignerSymbol = Symbol.for('solana:transaction-signer');
-
-const x402WalletAdapter = {
-  [TransactionSignerSymbol]: TransactionSignerSymbol,
-  
-  get address() {
-    if (!walletService.isWalletConnected() || !walletService.publicKey) {
-      throw new Error('Wallet must be connected before making payments');
-    }
-    return walletService.publicKey.toBase58();
-  },
-  
-  async signAndSendTransactions(transactions) {
-    if (!walletService.isWalletConnected()) {
-      throw new Error('Wallet not connected');
-    }
-    
-    const signatures = [];
-    for (const tx of transactions) {
-      const signature = await walletService.adapter.sendTransaction(tx, walletService.connection);
-      await walletService.connection.confirmTransaction(signature, 'confirmed');
-      signatures.push(signature);
-    }
-    return signatures;
-  },
-  
-  async signMessages(messages) {
-    if (!walletService.adapter?.signMessage) {
-      throw new Error('Wallet does not support message signing');
-    }
-    return Promise.all(messages.map(msg => walletService.adapter.signMessage(msg)));
-  },
-  
-  async signTransactions(transactions) {
-    if (!walletService.adapter?.signAllTransactions) {
-      throw new Error('Wallet does not support transaction signing');
-    }
-    return walletService.adapter.signAllTransactions(transactions);
-  }
-};
-
-const api = withPaymentInterceptor(
-    baseAxios,
-    x402WalletAdapter,
-    (error) => error?.response?.data?.accepts || [],
-    {
-        svmConfig: {
-            rpcUrl: import.meta.env.VITE_SOLANA_RPC_URL
-        }
-    }
-);
 
 api.interceptors.request.use(config => {
   const token = localStorage.getItem('business_token');
